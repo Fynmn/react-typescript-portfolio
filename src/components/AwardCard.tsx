0654c8660471b30beb6093ea978cdf5c8f21c3a7
Natalie Jane Pacificar <njanepacificar@wvsu.edu.ph>
2023-06-17 08:52:18 +0800

    fix: responsiveness of projects and awards

--- src/components/AwardCard.tsx
+++ src/components/AwardCard.tsx
@@ -29,18 +29,20 @@ const AwardCard: React.FC<IAwardCard> = ({
           }}
           className="hover:scale-105 hover:cursor-pointer"
         >
-          <div className="flex flex-col min-w-[200px] w-[280px] sm:w-[330px] p-8 gap-y-2 group">
+          <div className="flex flex-col min-w-[200px] w-[280px] sm:w-[330px] p-6 sm:p-8 gap-y-2 group">
             {/* <div className="flex flex-col min-w-[200px] w-96 p-8 gap-y-4 border-2 border-yellow-500 rounded-md group hover:bg-gradient-to-r hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 hover:shadow-lg hover:shadow-slate-400 hover:-translate-y-2 hover:scale-110 hover:border-0"> */}
             {/* <div className="flex relative w-[105px] h-[70px]">{icon}</div> */}
             <div className="flex flex-col gap-y-2 items-start ">
               {others ? (
                 <span className="flex items-center">
-                  <p className="text-yellow-900 font-bold mr-1">{placement}</p>
+                  <p className="text-yellow-900 font-bold mr-1 text-sm sm:text-base">
+                    {placement}
+                  </p>
                   <p className="text-yellow-900 font-light text-xs mr-1 mt-1">
                     {" "}
                     -{" "}
                   </p>
-                  <p className="text-yellow-900 font-light text-xs mt-1">
+                  <p className="text-yellow-900 font-light text-[10px] sm:text-xs mt-1">
                     {others}
                   </p>
                 </span>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -30,7 +30,7 @@ const Projects: React.FC<ProjectsProps> = ({ sectionRef }) => {
           Projects
         </h1>
 
-        <div className="flex flex-wrap items-center justify-evenly mx-16 gap-y-8 sm:gap-y-4 md:gap-y-2 lg:gap-y-0 pb-16">
+        <div className="flex flex-wrap items-center justify-evenly mx-16 gap-y-8 sm:gap-y-4 md:gap-y-2 lg:gap-y-0 gap-x-8 pb-16">
           {projects.map((project, i) => (
             <ProjectCard
               key={i}
