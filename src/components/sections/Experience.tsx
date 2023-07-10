d9e852ec48cb51e934b71c4d83b3df58078b25de
Natalie Jane Pacificar <njanepacificar@wvsu.edu.ph>
2023-07-11 01:51:31 +0800

    fix: error on Experience.jsx because of missing import and export statements

---
+++ src/components/sections/Experience.tsx
@@ -0,0 +1,9 @@
+import { SectionProps } from "./types";
+
+interface ExperienceProps extends SectionProps {}
+
+const Experience: React.FC<ExperienceProps> = ({ sectionRef }) => {
+  return <></>;
+};
+
+export default Experience;
