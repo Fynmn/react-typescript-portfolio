aa9c2344887926c83f3e6244ba4762203b6544e5
Natalie Jane Pacificar <njanepacificar@wvsu.edu.ph>
2023-07-20 22:16:25 +0800

    feat: added a test interface for the experience section

---
+++ src/data/experience.tsx
@@ -0,0 +1,19 @@
+
+export interface IExperienceCard {
+    job_title: string;
+    start_date: string;
+    end_date: string;
+    skills: string[];
+  }
+
+
+const awards: IExperienceCard[] = [
+  {
+    job_title: "Test",
+    start_date: "Test",
+    end_date: "Test",
+    skills: ["Test"],
+  },
+];
+
+export default awards;
