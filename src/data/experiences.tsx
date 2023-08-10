d60ee4f38a4bf21141cf2ce3e708258013159a0b
Natalie Jane Pacificar <njanepacificar@wvsu.edu.ph>
2023-08-10 16:02:41 +0800

    refactor: experience.tsx -> experiences.tsx and updated experiences data

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -1,25 +1,28 @@
 export interface IExperienceCard {
   job_title: string;
+  company: string;
   start_date: string;
   end_date: string;
   skills: string[];
 }
 
-const experience: IExperienceCard[] = [
+const experiences: IExperienceCard[] = [
   {
     job_title: "Software Engineer Trainee",
+    company: "Spring Valley Tech Crop.",
     start_date: "January 30, 2023",
-    end_date: "May 10, 2023",
+    end_date: " - May 10, 2023",
     skills: [
       "React, Next.js, JavaScript, TypeScript, HTML, CSS, TailwindCSS, Figma, Chart.js, Flutter, Dart, Git, GitLab, tRPC",
     ],
   },
   {
     job_title: "Front-end Developer",
+    company: "Alpha Four",
     start_date: "August 7, 2023",
     end_date: " - present",
     skills: ["Bubble.io"],
   },
 ];
 
-export default experience;
+export default experiences;
