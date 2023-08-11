ea4bcbb3bfa4a4b007a73bcac5bb876fc28d33ff
Natalie Jane Pacificar <njanepacificar@wvsu.edu.ph>
2023-08-11 23:28:53 +0800

    fix: lexical mistakes and updated stuff on experiences

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -1,27 +1,21 @@
-export interface IExperienceCard {
-  job_title: string;
-  company: string;
-  start_date: string;
-  end_date: string;
-  skills: string[];
-}
+import { IExperienceCardProps } from "@/components/ExperienceElement";
 
-const experiences: IExperienceCard[] = [
+const experiences: IExperienceCardProps[] = [
   {
     job_title: "Software Engineer Trainee",
     company: "Spring Valley Tech Corp.",
     start_date: "January 30, 2023",
-    end_date: " - May 10, 2023",
-    skills: [
-      "React, Next.js, JavaScript, TypeScript, HTML, CSS, TailwindCSS, Figma, Chart.js, Flutter, Dart, Git, GitLab, tRPC",
-    ],
+    end_date: "May 10, 2023",
+    description:
+      "Collaborated with a team of 5 to create a startup project from scratch. Designed the mobile app and web app in Figma as the lead UI/UX designer. Brought the designs to life as a frontend developer for both the mobile app and web app. Tech Stack & Tools: React, Next.js, JavaScript, TypeScript, HTML, CSS, TailwindCSS, Figma, Chart.js, Flutter, Dart, Git, GitLab, tRPC",
   },
   {
     job_title: "Front-end Developer",
     company: "Alpha Four",
     start_date: "August 7, 2023",
-    end_date: " - present",
-    skills: ["Bubble.io"],
+    end_date: undefined,
+    description:
+      "Creating high-quality no-code web applications for enterprises, characterized by seamless functionality, robust capabilities, and distinctive features. Tech Stack & Tools: Bubble.io, Linear, Notion, Slack",
   },
 ];
 
